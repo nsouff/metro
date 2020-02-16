d841e34539d9a4c4eeab6f1ce1a8e160bc440380
Nathan Souffan <souffan.nathan@gmail.com>
2020-02-16 17:27:39 +0100

    Add javadoc for Parser.java

--- src/main/java/fr/univparis/metro/Parser.java
+++ src/main/java/fr/univparis/metro/Parser.java
@@ -6,16 +6,21 @@ import java.io.*;
 public class Parser {
 
  /**
-  * the default weight for the weight graph in {@link loadFrom}
+  * The default weight between station
   */
   private static final Double defaultWeight = 90.0;
 
+ /**
+  * The default weight for in station change
+  */
   private static final Double defaultChangeStationWeight = 60.0;
 
  /**
   * Create a WGraph using a text file with stations on each line
+  * In station change are weighted by {@value #defaudefaultChangeStationWeight}
+  * Weight between station is {@value #defaultWeight}
   * @param f is the txt file wich contain the station
-  * @return the WGraph with every weight = {@link defaultWeight}
+  * @return the WGraph that correspond to the file
   */
   public static WGraph<Station> loadFrom (File f) throws IOException {
     Scanner sc = new Scanner (f);
@@ -43,6 +48,17 @@ public class Parser {
     return g;
   }
 
+ /**
+  * Used when there is a fork in the subway,
+  * Create the two branch while there is no "]" in the {@link java.util.Scanner}
+  * || is the separator for the two branch
+  * @param g The graph that has been initialized and where the fork will be added
+  * @param sc The Scanner with stations on every line
+  * @param start The station before the fork, if it's null then the line is starting by a fork
+  * @param line the line in wich the fork is
+  * @param createdStation a Set to know wich Station has already been created (juste name of the station)
+  * @return null if the fork ends the line or the station that linked the two branch
+  */
   private static Station fork(WGraph<Station> g, Scanner sc, Station start, String line, HashSet<String> createdStation) {
     String s;
     Station endOfFirst = start;
@@ -71,6 +87,19 @@ public class Parser {
     return new Station(s, line);
   }
 
+
+ /**
+  * Used when there is a cycle in the subway
+  * Create the cylce that has only one direction
+  * / is the character indicated in the {@link java.util.Scanner} to know where starts the other branche of the cycle
+  * } is the character that indicates the end of the cycle
+  * @param g The graph that has been initialized and where the fork will be added
+  * @param sc The Scanner with stations on every line
+  * @param start The station before the start of the cycle
+  * @param line The line in wich the cycle is
+  * @param createdStation a Set to know wich Station has already been created (juste name of the station)
+  * @return the station that is right after the end of the cycle
+  */
   private static Station cycle(WGraph<Station> g, Scanner sc, Station start, String line, HashSet<String> createdStation) {
     if (start == null) throw new IllegalStateException("Cycle can't start with a null station");
     String s;
@@ -106,13 +135,16 @@ public class Parser {
 
 
  /**
-  * Add a {@link Station} named s to the {@link Wgraph} g if s is a station name and if it's not in g yet
-  * if s is not, the function just return prec without doing anything
-  * if s is a station name, it will add an Edge between prec and the station
-  * @param g the {@link WGraph} into wich we want to add the station
-  * @param prec the station before the station name s, the two vertex will be linked by the weight {@link defaultWeight}
-  * @param s the name of the station
-  * @return the Station we Station we just created or prec if we didn't
+  * Add edge beteen Station prec and the line named s in the line line
+  * If a station with the same name has already been created, it also created change of line edges
+  * @param g The graph in wich we want to add Edge
+  * @param prec The station before the station we want to add
+  * @param s The name of the station
+  * @param line The line of the added Station
+  * @param ahead True if we want to add an edge that start at prec and goes to the created station
+  * @param behind True if we want to add an edge that start at the created station and goes to prec
+  * @param createdStation The set of the station already created, to know if we should add in station Edge
+  * @return The new Station(s, line)
   */
   private static Station addNextStation(WGraph<Station> g, Station prec, String s, String line, boolean ahead, boolean behind, HashSet<String> createdStation) {
     Station act = new Station(s, line);
