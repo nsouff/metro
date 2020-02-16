46a7e54b78d38c990e0847fd4265f50db2c6d4fe
Nathan Souffan <souffan.nathan@gmail.com>
2020-02-16 17:30:27 +0100

    Add javadoc for Station.java

--- src/main/java/fr/univparis/metro/Station.java
+++ src/main/java/fr/univparis/metro/Station.java
@@ -4,7 +4,12 @@ public class Station {
   private final String name;
   private final String line;
 
-
+ /**
+  * Create a station name n in the line l
+  * @param n The name of the station
+  * @param l The line of the station
+  * @return The station named n in the line l
+  */
   Station(String n, String l) {
     if (l == null || l.isEmpty() || n == null || n.isEmpty())
       throw new IllegalArgumentException("Station name and line can't be null or empty");
@@ -12,8 +17,16 @@ public class Station {
     line = l;
   }
 
+ /**
+  * Return the name of the station
+  * @return the name of the station
+  */
   public String getName() {return name;}
 
+ /**
+  * Return the line of the station
+  * @return the line of the station
+  */
   public String getLine() {return line;}
 
   @Override
@@ -36,5 +49,10 @@ public class Station {
     return name.hashCode();
   }
 
+ /**
+  * Return true if s and this station have the same name
+  * @param s The station we want to test if they have the same name
+  * @return true if and only if s.name.equals(name)
+  */
   public boolean sameName(Station s) {return s.name.equals(name);}
 }
