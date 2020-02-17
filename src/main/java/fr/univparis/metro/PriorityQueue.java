445993649db13b1545ed5118ba389270e5db6777
Romain Bouarah <Rom.Bou@hotmail.fr>
2020-02-17 12:31:15 +0100

    Add add method in PriorityQueue class

--- src/main/java/fr/univparis/metro/PriorityQueue.java
+++ src/main/java/fr/univparis/metro/PriorityQueue.java
@@ -8,25 +8,48 @@ import java.lang.Integer;
 public class PriorityQueue<E> {
 
     private class Pair {
-
+        E val;
+	Double key;
+	
 	public Pair(E v, Double k) {
 	    val = v;
 	    key = k;
 	}
-	
-        E val;
-	Double key;
     }
 	    
     private ArrayList<Pair> tree;
     private HashMap<E, Integer> index;
-
-
+    
     public PriorityQueue() {
 	tree = new ArrayList<Pair>();
 	index = new HashMap<E, Integer>();
     }
 
 
+    /** 
+     * Inserts the specified element into this priority queue given a key (priority).
+     * @param val the element to add
+     * @param key the priority of val
+     */ 
+    public boolean add(E val, Double key) {
+	if( index.containsKey(val) )
+	    return false;
+
+	Pair entry = new Pair(val,key);
+	tree.add( entry );
     
+	int i = tree.size() - 1;
+
+	/* Move entry up in the tree, as long as needed; used to restore heap condition after insertion. */
+	while(i >= 1 && tree.get((i-1)/2).key > key) {
+	    tree.set(i, tree.get((i-1)/2)); 
+	    index.put(tree.get((i-1)/2).val, i);
+
+	    i = (i-1)/2;	    
+	}
+	
+	tree.set(i, entry);
+	index.put(val, i);
+	return true;
+    }    
 }
---
+++ src/test/java/fr/univparis/metro/PriorityQueueTest.java
@@ -0,0 +1,38 @@
+package fr.univparis.metro;
+
+import java.lang.Double;
+import java.lang.Integer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class PriorityQueueTest {
+
+    PriorityQueue<String> pQueue;
+
+    @Before
+    public void initPriorityQueue() {
+	pQueue = new PriorityQueue<String>();
+
+	pQueue.add("A", 5.0);
+
+	pQueue.add("B", 10.0);
+	pQueue.add("C", 7.0);
+
+	pQueue.add("D", 12.0);
+	pQueue.add("E", 15.0);
+	pQueue.add("F", 10.0);
+	pQueue.add("G", 8.0);
+    }
+
+    @Test
+    public void addTest() {
+	assertTrue( pQueue.add("H",17.0));
+
+	assertFalse( pQueue.add("H",0.0));
+    }
+    
+}
