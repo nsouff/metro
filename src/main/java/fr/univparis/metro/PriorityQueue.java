1d2088e23af6e7114a841018cb45dcf3eb371093
Romain Bouarah <Rom.Bou@hotmail.fr>
2020-02-17 11:41:42 +0100

    Add empty PriorityQueue class

---
+++ src/main/java/fr/univparis/metro/PriorityQueue.java
@@ -0,0 +1,25 @@
+package fr.univparis.metro;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.lang.Double;
+import java.lang.Integer;
+
+public class PriorityQueue<E> {
+
+    private class Pair {
+
+	public Pair(E v, Double k) {
+	    val = v;
+	    key = k;
+	}
+	
+        E val;
+	Double key;
+    }
+	    
+    private ArrayList<Pair> tree;
+    private HashMap<E, Integer> index;
+
+
+}
