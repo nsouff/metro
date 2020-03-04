eac2986eb365b48e1a1f26b34acb5059f2a34074
Lucas Ketels <lucas.ketels@etu.univ-paris-diderot.fr>
2020-03-04 11:32:36 +0100

    Add genericity in limitedconnectionsearch

--- src/main/java/fr/univparis/metro/LimitedConnectionSearch.java
+++ src/main/java/fr/univparis/metro/LimitedConnectionSearch.java
@@ -2,21 +2,22 @@ package fr.univparis.metro;
 
 import java.util.Set;
 import java.util.ArrayList;
-import java.util.AbstractMap.SimpleEntry;
+import java.util.List;
+import java.util.Map.Entry;
 
-public class LimitedConnectionSearch{
+public class LimitedConnectionSearch {
 
-    public static WGraph createGraphLine(WGraph g, String line){
-        WGraph ret = new WGraph();
-        ArrayList<SimpleEntry<Station, ArrayList<Pair<Station, Double>>>> tmp = new ArrayList<SimpleEntry<Station, ArrayList<Pair<Station, Double>>>>();
-        Set<SimpleEntry<Station, ArrayList<Pair<Station, Double>>>> set = g.getWGraph().entrySet();
-        for(SimpleEntry<Station, ArrayList<Pair<Station, Double>>> m : set){
-            if(m.getKey().getName().equals(line)) tmp.add(m);
+    public static WGraph<Station> createGraphLine(WGraph<Station> g, String line) {
+        WGraph<Station> ret = new WGraph<Station>();
+        ArrayList<Entry<Station, List<Pair<Station, Double>>>> tmp = new ArrayList<Entry<Station, List<Pair<Station, Double>>>>();
+        Set<Entry<Station, List<Pair<Station, Double>>>> set = g.getWGraph().entrySet();
+        for(Entry<Station, List<Pair<Station, Double>>> m : set){
+            if(m.getKey().getLine().equals(line)) tmp.add(m);
         }
         return ret;
     }
 
-    public static Double[][] directMatrix(WGraph g){
+    public static Double[][] directMatrix(WGraph<Station> g){
         Double[][] ret = new Double[g.nbVertex()][g.nbVertex()];
         return ret;
     }
