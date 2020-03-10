64ef61a9b17ec8175e7c2b087c00d15eaf5e3b6f
Lucas Ketels <lucas.ketels@etu.univ-paris-diderot.fr>
2020-03-10 13:52:39 +0100

    Begin writing the algorithm to limit connexions

--- src/main/java/fr/univparis/metro/LimitedConnectionSearch.java
+++ src/main/java/fr/univparis/metro/LimitedConnectionSearch.java
@@ -1,24 +1,40 @@
 package fr.univparis.metro;
 
-import java.util.Set;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map.Entry;
-
 public class LimitedConnectionSearch {
 
-    public static WGraph<Station> createGraphLine(WGraph<Station> g, String line) {
+    /**
+     * @param g the graph from which we suppress some stations.
+     * @param line suppress all the sations that are not on this line.
+     * @return a graph whose sations are all on the same line.
+     * FIXME: the complexity of this method is O(n^3) (in the worst case), n being the number of stations in the graph g.
+     */
+    public static WGraph<Station> createGraphLine(WGraph<Station> g, String line){
         WGraph<Station> ret = new WGraph<Station>();
-        ArrayList<Entry<Station, List<Pair<Station, Double>>>> tmp = new ArrayList<Entry<Station, List<Pair<Station, Double>>>>();
-        Set<Entry<Station, List<Pair<Station, Double>>>> set = g.getWGraph().entrySet();
-        for(Entry<Station, List<Pair<Station, Double>>> m : set){
-            if(m.getKey().getLine().equals(line)) tmp.add(m);
+        ret.getWGraph().putAll(g.getWGraph());
+        for(Station s : g.getVertices()){
+            if(!s.getLine().equals(line)) ret.deleteVertex(s);
         }
         return ret;
     }
 
-    public static Double[][] directMatrix(WGraph<Station> g){
-        Double[][] ret = new Double[g.nbVertex()][g.nbVertex()];
-        return ret;
+    static int[][] floyd(int[][] c) {
+        int n = c.length;
+        int[][] d = new int[n][n];
+        for (int i=0; i<n; i++) {
+          for (int j=0; j<n; j++) {
+            d[i][j] = c[i][j];
+            }
+        }
+        for (int k=0; k<n; k++) {
+            for (int i=0; i<n; i++) {
+                for (int j=0; j<n; j++) {
+                    int dik = d[i][k], dkj = d[k][j];
+                    if (dik == Integer.MAX_VALUE || dkj == Integer.MAX_VALUE) continue;
+                    int u = dik + dkj;
+                    if (u < d[i][j]) d[i][j] = u;
+                }
+            }
+        }
+        return d;
     }
 }
