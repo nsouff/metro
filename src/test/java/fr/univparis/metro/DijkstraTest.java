744c46001cb6535e1ad9415a2b8c5df4991f2d71
Nathan Souffan <souffan.nathan@gmail.com>
2020-03-11 11:41:45 +0100

    Add Test for each Pair of start and end Station

--- src/test/java/fr/univparis/metro/DijkstraTest.java
+++ src/test/java/fr/univparis/metro/DijkstraTest.java
@@ -22,41 +22,35 @@ public class DijkstraTest{
     Station a2                  = new Station("Châtelet", "14");
     Station a3                  = new Station("Châtelet", "4");
     Station a4                  = new Station("Saint-Placide", "4");
-    HashMap<Station, Station> stat = new HashMap<Station, Station>();
+    HashMap<Station, Station> prev = new HashMap<Station, Station>();
     HashMap<Station, Double> dist  = new HashMap<Station, Double>();
-    Dijkstra.shortestPath(w, a1, stat, dist);
+    Station laumS = new Station("Laumière", "Meta Station Start");
+    Station mdiE = new Station("Mairie d'Issy", "Meta Station End");
+
+    Dijkstra.shortestPath(w, a1, prev, dist);
     assertEquals((Double) 270.0,  dist.get(a2));
     assertEquals((Double) 330.0, dist.get(a3));
     assertEquals((Double) 0.0, dist.get(a1));
     assertEquals((Double) 840.0, dist.get(a4));
-    assertEquals("Station: Montparnasse - Bienvenüe, 4", stat.get(a4).toString());
-    assertEquals("Station: Châtelet, 14", stat.get(a3).toString());
-    assertEquals("Station: Pyramides, 14", stat.get(a2).toString());
-
-
-    // Stations that had bugs
-    Station laumS = new Station("Laumière", "Meta Station Start");
-    Station mdiS = new Station("Mairie d'Issy", "Meta Station Start");
-    Station mdiE = new Station("Mairie d'Issy", "Meta Station End");
-    Station aglcS = new Station("Asnières - Gennevilliers - Les Courtilles", "Meta Station Start");
-    Station cpS = new Station("Créteil - Préfecture", "Meta Station Start");
-    Station gS = new Station("Gallieni", "Meta Station Start");
-    Station afS = new Station("Anatole France", "Meta Station Start");
-    Dijkstra.shortestPath(w, laumS, stat, dist);
+    assertEquals("Station: Montparnasse - Bienvenüe, 4", prev.get(a4).toString());
+    assertEquals("Station: Châtelet, 14", prev.get(a3).toString());
+    assertEquals("Station: Pyramides, 14", prev.get(a2).toString());
+    Dijkstra.shortestPath(w, laumS, prev, dist);
     assertEquals((Double) 2340.0, dist.get(mdiE));
 
-    /*
-    Station[] set = {laumS, mdiS, aglcS, cpS, gS, afS};
+    Set<Station> set = w.getVertices();
     for (Station st : set) {
-      Dijkstra.shortestPath(w, st, stat, dist);
-      for (Station s : w.getVertices()) {
-        if (s.getLine() != "Meta Station Start"){
+      if (! st.getLine().equals("Meta Station Start"))
+        continue;
+      Dijkstra.shortestPath(w, st, prev, dist);
+      for (Station s : set) {
+        if (s.getLine().equals("Meta Station End")){
           Double d = dist.get(s);
-          assertTrue("" + st + s, d != Double.NaN && d != Double.POSITIVE_INFINITY);
-          assertNotNull(stat.get(s));
+          assertNotEquals(Double.NaN, d);
+          assertNotEquals(Double.POSITIVE_INFINITY, d);
+          assertNotNull(prev.get(s));
         }
       }
     }
-    */
   }
 }
