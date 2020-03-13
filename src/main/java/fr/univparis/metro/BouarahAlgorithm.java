0366e3568b6c8f25a128126f67f274ea324bbe09
Romain Bouarah <Rom.Bou@hotmail.fr>
2020-03-13 13:29:34 +0100

    Add javadoc for shortestPath in BouarahAlgorithm

--- src/main/java/fr/univparis/metro/BouarahAlgorithm.java
+++ src/main/java/fr/univparis/metro/BouarahAlgorithm.java
@@ -7,6 +7,12 @@ import java.util.function.BiPredicate;
 
 public class BouarahAlgorithm {
 
+    /**
+     * Initialize *dist* and *priQueue*
+     * For each pair <V,k> where V is a vertex in *graph* and k is an integer between 0 and *limit*
+     * <V,k> is added to *dist* with +infininty if V != *start* else 0.0
+     * <V,k> is added into *priQueue* with priority dist[<V,k>]
+     */
     private static <T> void initShortestPath(WGraph<T> graph, T start, int limit, HashMap<Pair<T, Integer>, Double> dist, PriorityQueue<Pair<T,Integer>> priQueue) {
 	Pair<T, Integer> p;
 	for ( T vertex : graph.getVertices() ) {
@@ -31,7 +37,15 @@ public class BouarahAlgorithm {
     }
 
     
-
+    /**
+     * Find the shortest path with a
+     * @param graph A weighted and oriented graph
+     * @param start The start of research
+     * @param limit The number of passage between equivalence classes allowed
+     * @param equivalentRelation An equivalence relation i.e. a binary relation that is reflexive, symmetric and transitive 
+     * @param prev A HashMap associating a pair <V,k> to the previous pair <W,i>. W is a neighbor of V and i=k iff W and V are in the same equivalence classes
+     * @param dist A HashMap associating a pair <V,k> to his distance from start with a shortest path between <start,0> and <V,k>
+     */
     public static <T> void shortestPath (WGraph<T> graph, T start, int limit, BiPredicate<T,T> equivalenceRelation,
 					 HashMap<Pair<T, Integer>, Pair<T, Integer>> prev, HashMap<Pair<T, Integer>, Double> dist) {
 	PriorityQueue<Pair<T, Integer>> priQueue = new PriorityQueue<Pair<T, Integer>>();
