b6efe4144c0ac0766d2ecb4c99475c074c3304b8
mlangd <matthieu.langdorph@gmail.com>
2020-03-14 01:02:33 +0100

    Add the javadoc on the class Statistics.java

--- src/main/java/fr/univparis/metro/Statistics.java
+++ src/main/java/fr/univparis/metro/Statistics.java
@@ -2,8 +2,20 @@ package fr.univparis.metro;
 import java.util.HashMap;
 import java.util.function.Predicate;
 
+/**
+  * Class for statistics about our networks
+  *
+  */
+
 public class Statistics{
 
+  /**
+  * This function give the two stations the more distant on a network
+  * @param g the graph representing the network
+  * @param p1 is the predicate to applicate Dijkstra to an element of the graph
+  * @param p2 is the predicate to comparate the weight between two elements of the graph
+  * @return a pair of pair of elements and the weight between these two elements which are the two elements the more distant on the graph
+  */
   public static <T> Pair<Pair<T, T>, Double> mostDistantStations(WGraph<T> g, Predicate<T> p1, Predicate<T> p2){
     Double biggestTime = 0.;
     Pair<T, T> stations = new Pair<T,T>(null, null);
@@ -23,7 +35,6 @@ public class Statistics{
 	    dist.clear();
     }
     res = new Pair<Pair<T,T>, Double>(stations, biggestTime);
-    System.out.println("Les deux stations les plus éloignées du réseau\nStation de départ : "+res.getObj().getObj()+"\nStation d'arrivée : "+res.getObj().getValue()+"\nDurée : "+res.getValue());
     return res;
   }
 }
