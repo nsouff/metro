70accf18ee25655c780962d9514931f5e7fce6e4
mlangd <matthieu.langdorph@gmail.com>
2020-03-19 18:54:33 +0100

    Split the test for each function of Statistics

--- src/test/java/fr/univparis/metro/StatisticsTest.java
+++ src/test/java/fr/univparis/metro/StatisticsTest.java
@@ -11,17 +11,21 @@ public class StatisticsTest{
 
     @BeforeClass
     public static void loadFile() throws IOException{
-	URL url = ParserTest.class.getResource("/liste.txt");
-  	File f = new File(url.getFile());
-  	g = Parser.loadFrom(f);
+      URL url = ParserTest.class.getResource("/liste.txt");
+      File f = new File(url.getFile());
+      g = Parser.loadFrom(f);
     }
 
     @Test
     public void mostDistantStationsTest(){
-	Pair<Pair<Station, Station>, Double> res= Statistics.mostDistantStations(g, (s -> !s.getLine().equals("Meta Station Start")), (t -> t.getLine().equals("Meta Station End")));
-	assertEquals((Double)3270. , res.getValue());
-	assertEquals("Pont de Sèvres", res.getObj().getObj().toString());
-	assertEquals("Créteil - Préfecture", res.getObj().getValue().toString());
-	assertEquals(3, Statistics.minimumCorrespondence(g, (s -> s.getLine().equals("Meta Station Start")), s -> s.getLine().equals("Meta Station End") , (Station s1, Station s2) -> s1.getLine().equals(s2.getLine()) || s1.getLine().startsWith("Meta Station") || s2.getLine().startsWith("Meta Station")));
+      Pair<Pair<Station, Station>, Double> res= Statistics.mostDistantStations(g, (s -> !s.getLine().equals("Meta Station Start")), (t -> t.getLine().equals("Meta Station End")));
+      assertEquals((Double)3270. , res.getValue());
+      assertEquals("Pont de Sèvres", res.getObj().getObj().toString());
+      assertEquals("Créteil - Préfecture", res.getObj().getValue().toString());
+    }
+
+    @Test
+    public void minimumCorrespondenceTest(){
+      assertEquals(3, Statistics.minimumCorrespondence(g, (s -> s.getLine().equals("Meta Station Start")), s -> s.getLine().equals("Meta Station End") , (Station s1, Station s2) -> s1.getLine().equals(s2.getLine()) || s1.getLine().startsWith("Meta Station") || s2.getLine().startsWith("Meta Station")));
     }
 }
