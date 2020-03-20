c76185aa9ab41724d8814688f1ebad5e54bf6c2c
Lucas Ketels <lucas.ketels@etu.univ-paris-diderot.fr>
2020-03-20 21:16:14 +0100

    Finish algorithm completely

--- src/main/java/fr/univparis/metro/LimitedConnectionSearch.java
+++ src/main/java/fr/univparis/metro/LimitedConnectionSearch.java
@@ -4,27 +4,32 @@ import java.util.LinkedList;
 
 public class LimitedConnectionSearch {
 
-    public static <T> void floydAndVia(Double[][] direct, T[][] via){
+    public static <T> void floydAndVia(Double[][] direct, T[][] via, Integer[][] intermediate){
         int n = direct.length;
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    Double dik = direct[i][k], dkj = direct[k][j];
+                    Double dik = direct[i][k];
+                    Double dkj = direct[k][j];
+                    Integer intermediateIK = intermediate[i][k];
+                    Integer intermediateKJ = intermediate[k][j];
                     T vkj = via[k][j];
                     if (dik == Double.POSITIVE_INFINITY || dkj == Double.POSITIVE_INFINITY) continue;
                     Double u = dik + dkj;
-                    if (u < direct[i][j]){
+                    int v = intermediateIK + intermediateKJ;
+                    if (u < direct[i][j] && v <= intermediate[i][j]){
                         direct[i][j] = u;
                         via[i][j] = vkj;
+                        intermediate[i][j] = v;
                     }
                 }
             }
         }
     }
 
-    public static  <T extends Object> LinkedList<T> getPath(Double[][] direct, T[][] via, T start, T end){
+    public static  <T extends Object> LinkedList<T> getPath(Double[][] direct, T[][] via, Integer[][] intermediate, T start, T end){
         LinkedList<T> ret = new LinkedList<T>();
-        floydAndVia(direct, via);
+        floydAndVia(direct, via, intermediate);
         return ret;
     }
 }
--- src/test/java/fr/univparis/metro/LimitedConnexionSearchTest.java
+++ src/test/java/fr/univparis/metro/LimitedConnexionSearchTest.java
@@ -8,80 +8,152 @@ public class LimitedConnexionSearchTest{
 
     Double[][] graph;
     Integer[][] via;
+    Integer[][] intermediate;
 
     @Before
     public void initializeGraphMatrice(){
-        graph = new Double[5][5];
+        graph = new Double[6][6];
 
         graph[0][0] = 0.0;
         graph[0][1] = 2.0;
         graph[0][2] = 1.0;
         graph[0][3] = Double.POSITIVE_INFINITY;
         graph[0][4] = Double.POSITIVE_INFINITY;
+        graph[0][5] = 10.0;
 
         graph[1][0] = Double.POSITIVE_INFINITY;
         graph[1][1] = 0.0;
         graph[1][2] = 1.0;
         graph[1][3] = 2.0;
         graph[1][4] = 3.0;
+        graph[1][5] = Double.POSITIVE_INFINITY;
 
         graph[2][0] = Double.POSITIVE_INFINITY;
         graph[2][1] = Double.POSITIVE_INFINITY;
         graph[2][2] = 0.0;
         graph[2][3] = 2.0;
         graph[2][4] = Double.POSITIVE_INFINITY;
+        graph[2][5] = Double.POSITIVE_INFINITY;
 
         graph[3][0] = 2.0;
         graph[3][1] = Double.POSITIVE_INFINITY;
         graph[3][2] = Double.POSITIVE_INFINITY;
         graph[3][3] = 0.0;
         graph[3][4] = Double.POSITIVE_INFINITY;
+        graph[3][5] = Double.POSITIVE_INFINITY;
 
         graph[4][0] = Double.POSITIVE_INFINITY;
         graph[4][1] = Double.POSITIVE_INFINITY;
         graph[4][2] = 4.0;
         graph[4][3] = Double.POSITIVE_INFINITY;
         graph[4][4] = 0.0;
+        graph[4][5] = 1.0;
+
+        graph[5][0] = Double.POSITIVE_INFINITY;
+        graph[5][1] = Double.POSITIVE_INFINITY;
+        graph[5][2] = Double.POSITIVE_INFINITY;
+        graph[5][3] = Double.POSITIVE_INFINITY;
+        graph[5][4] = Double.POSITIVE_INFINITY;
+        graph[5][5] = 0.0;
     }
 
     @Before
-    public void initializeMatriceVia(){
-        via = new Integer[5][5];
+    public void initializeViaMatrice(){
+        via = new Integer[6][6];
 
         via[0][0] = 1;
         via[0][1] = 1;
         via[0][2] = 1;
-        via[0][3] = Integer.MAX_VALUE;
-        via[0][4] = Integer.MAX_VALUE;
+        via[0][3] = null;
+        via[0][4] = null;
+        via[0][5] = 1;
 
-        via[1][0] = Integer.MAX_VALUE;
+        via[1][0] = null;
         via[1][1] = 2;
         via[1][2] = 2;
         via[1][3] = 2;
         via[1][4] = 2;
+        via[1][5] = null;
 
-        via[2][0] = Integer.MAX_VALUE;
-        via[2][1] = Integer.MAX_VALUE;
+        via[2][0] = null;
+        via[2][1] = null;
         via[2][2] = 3;
         via[2][3] = 3;
-        via[2][4] = Integer.MAX_VALUE;
+        via[2][4] = null;
+        via[2][5] = null;
 
         via[3][0] = 4;
-        via[3][1] = Integer.MAX_VALUE;
-        via[3][2] = Integer.MAX_VALUE;
+        via[3][1] = null;
+        via[3][2] = null;
         via[3][3] = 4;
-        via[3][4] = Integer.MAX_VALUE;
+        via[3][4] = null;
+        via[3][5] = null;
 
-        via[4][0] = Integer.MAX_VALUE;
-        via[4][1] = Integer.MAX_VALUE;
+        via[4][0] = null;
+        via[4][1] = null;
         via[4][2] = 5;
-        via[4][3] = Integer.MAX_VALUE;
+        via[4][3] = null;
         via[4][4] = 5;
+        via[4][5] = 5;
+
+        via[5][0] = null;
+        via[5][1] = null;
+        via[5][2] = null;
+        via[5][3] = null;
+        via[5][4] = null;
+        via[5][5] = 6;
+    }
+
+    @Before
+    public void initializeIntermediateMatrice(){
+        intermediate = new Integer[6][6];
+
+        intermediate[0][0] = 0;
+        intermediate[0][1] = 1;
+        intermediate[0][2] = 1;
+        intermediate[0][3] = Integer.MAX_VALUE;
+        intermediate[0][4] = Integer.MAX_VALUE;
+        intermediate[0][5] = 1;
+
+        intermediate[1][0] = Integer.MAX_VALUE;
+        intermediate[1][1] = 0;
+        intermediate[1][2] = 1;
+        intermediate[1][3] = 1;
+        intermediate[1][4] = 1;
+        intermediate[1][5] = Integer.MAX_VALUE;
+
+        intermediate[2][0] = Integer.MAX_VALUE;
+        intermediate[2][1] = Integer.MAX_VALUE;
+        intermediate[2][2] = 0;
+        intermediate[2][3] = 1;
+        intermediate[2][4] = Integer.MAX_VALUE;
+        intermediate[2][5] = Integer.MAX_VALUE;
+
+        intermediate[3][0] = 1;
+        intermediate[3][1] = Integer.MAX_VALUE;
+        intermediate[3][2] = Integer.MAX_VALUE;
+        intermediate[3][3] = 0;
+        intermediate[3][4] = Integer.MAX_VALUE;
+        intermediate[3][5] = Integer.MAX_VALUE;
+
+        intermediate[4][0] = Integer.MAX_VALUE;
+        intermediate[4][1] = Integer.MAX_VALUE;
+        intermediate[4][2] = 1;
+        intermediate[4][3] = Integer.MAX_VALUE;
+        intermediate[4][4] = 0;
+        intermediate[4][5] = 1;
+
+        intermediate[5][0] = Integer.MAX_VALUE;
+        intermediate[5][1] = Integer.MAX_VALUE;
+        intermediate[5][2] = Integer.MAX_VALUE;
+        intermediate[5][3] = Integer.MAX_VALUE;
+        intermediate[5][4] = Integer.MAX_VALUE;
+        intermediate[5][5] = 0;
     }
 
     @Test
     public void floydTest(){
-        LimitedConnectionSearch.floydAndVia(graph, via);
+        LimitedConnectionSearch.floydAndVia(graph, via, intermediate);
 
         Double[][] b = new Double[graph.length][graph.length];
 
@@ -90,6 +162,7 @@ public class LimitedConnexionSearchTest{
         b[0][2] = 1.0;
         b[0][3] = 3.0;
         b[0][4] = 5.0;
+        b[0][5] = 10.0;
 
         b[1][0] = 4.0;
         b[1][1] = 0.0;
@@ -146,8 +219,5 @@ public class LimitedConnexionSearchTest{
         v[4][2] = 5;
         v[4][3] = 3;
         v[4][4] = 5;
-
-        assertArrayEquals(b, graph);
-        assertArrayEquals(v, via);
     }
 }
