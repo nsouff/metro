df8f0acaf63b1a14e4b1a60fcb05246cecacdac6
Lucas Ketels <lucas.ketels@etu.univ-paris-diderot.fr>
2020-03-20 01:51:07 +0100

    Finish writing the algorithm and the tests for the algorithm

--- src/main/java/fr/univparis/metro/LimitedConnectionSearch.java
+++ src/main/java/fr/univparis/metro/LimitedConnectionSearch.java
@@ -1,25 +1,30 @@
 package fr.univparis.metro;
 
+import java.util.LinkedList;
+
 public class LimitedConnectionSearch {
 
-    public static Double[][] floyd(Double[][] graph, int start, int end){
-        int n = graph.length;
-        Double[][] d = new Double[n][n];
-        for (int i = 0; i < n; i++) {
-          for (int j = 0; j < n; j++) {
-            d[i][j] = graph[i][j];
-            }
-        }
+    public static <T> void floydAndVia(Double[][] direct, T[][] via){
+        int n = direct.length;
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    Double dik = d[i][k], dkj = d[k][j];
+                    Double dik = direct[i][k], dkj = direct[k][j];
+                    T vkj = via[k][j];
                     if (dik == Double.POSITIVE_INFINITY || dkj == Double.POSITIVE_INFINITY) continue;
                     Double u = dik + dkj;
-                    if (u < d[i][j]) d[i][j] = u;
+                    if (u < direct[i][j]){
+                        direct[i][j] = u;
+                        via[i][j] = vkj;
+                    }
                 }
             }
         }
-        return d;
+    }
+
+    public static  <T extends Object> LinkedList<T> getPath(Double[][] direct, T[][] via, T start, T end){
+        LinkedList<T> ret = new LinkedList<T>();
+        floydAndVia(direct, via);
+        return ret;
     }
 }
--- src/test/java/fr/univparis/metro/LimitedConnexionSearchTest.java
+++ src/test/java/fr/univparis/metro/LimitedConnexionSearchTest.java
@@ -6,58 +6,148 @@ import static org.junit.Assert.*;
 
 public class LimitedConnexionSearchTest{
 
-    Double[][] g;
+    Double[][] graph;
+    Integer[][] via;
 
     @Before
     public void initializeGraphMatrice(){
-        g = new Double[4][4];
+        graph = new Double[5][5];
 
-        g[0][0] = 0.0;
-        g[0][1] = 2.0;
-        g[0][2] = 4.0;
-        g[0][3] = Double.POSITIVE_INFINITY;
+        graph[0][0] = 0.0;
+        graph[0][1] = 2.0;
+        graph[0][2] = 1.0;
+        graph[0][3] = Double.POSITIVE_INFINITY;
+        graph[0][4] = Double.POSITIVE_INFINITY;
 
-        g[1][0] = Double.POSITIVE_INFINITY;
-        g[1][1] = 0.0;
-        g[1][2] = 3.0;
-        g[1][3] = Double.POSITIVE_INFINITY;
+        graph[1][0] = Double.POSITIVE_INFINITY;
+        graph[1][1] = 0.0;
+        graph[1][2] = 1.0;
+        graph[1][3] = 2.0;
+        graph[1][4] = 3.0;
 
-        g[2][0] = Double.POSITIVE_INFINITY;
-        g[2][1] = Double.POSITIVE_INFINITY;
-        g[2][2] = 0.0;
-        g[2][3] = 5.0;
+        graph[2][0] = Double.POSITIVE_INFINITY;
+        graph[2][1] = Double.POSITIVE_INFINITY;
+        graph[2][2] = 0.0;
+        graph[2][3] = 2.0;
+        graph[2][4] = Double.POSITIVE_INFINITY;
 
-        g[3][0] = 1.0;
-        g[3][1] = Double.POSITIVE_INFINITY;
-        g[3][2] = Double.POSITIVE_INFINITY;
-        g[3][3] = 0.0;
+        graph[3][0] = 2.0;
+        graph[3][1] = Double.POSITIVE_INFINITY;
+        graph[3][2] = Double.POSITIVE_INFINITY;
+        graph[3][3] = 0.0;
+        graph[3][4] = Double.POSITIVE_INFINITY;
+
+        graph[4][0] = Double.POSITIVE_INFINITY;
+        graph[4][1] = Double.POSITIVE_INFINITY;
+        graph[4][2] = 4.0;
+        graph[4][3] = Double.POSITIVE_INFINITY;
+        graph[4][4] = 0.0;
+    }
+
+    @Before
+    public void initializeMatriceVia(){
+        via = new Integer[5][5];
+
+        via[0][0] = 1;
+        via[0][1] = 1;
+        via[0][2] = 1;
+        via[0][3] = Integer.MAX_VALUE;
+        via[0][4] = Integer.MAX_VALUE;
+
+        via[1][0] = Integer.MAX_VALUE;
+        via[1][1] = 2;
+        via[1][2] = 2;
+        via[1][3] = 2;
+        via[1][4] = 2;
+
+        via[2][0] = Integer.MAX_VALUE;
+        via[2][1] = Integer.MAX_VALUE;
+        via[2][2] = 3;
+        via[2][3] = 3;
+        via[2][4] = Integer.MAX_VALUE;
+
+        via[3][0] = 4;
+        via[3][1] = Integer.MAX_VALUE;
+        via[3][2] = Integer.MAX_VALUE;
+        via[3][3] = 4;
+        via[3][4] = Integer.MAX_VALUE;
+
+        via[4][0] = Integer.MAX_VALUE;
+        via[4][1] = Integer.MAX_VALUE;
+        via[4][2] = 5;
+        via[4][3] = Integer.MAX_VALUE;
+        via[4][4] = 5;
     }
 
     @Test
     public void floydTest(){
-        Double[][] t = LimitedConnectionSearch.floyd(g, 0, 2);
-        Double[][] a = new Double[g.length][g.length];
+        LimitedConnectionSearch.floydAndVia(graph, via);
 
-        a[0][0] = 0.0;
-        a[0][1] = 2.0;
-        a[0][2] = 4.0;
-        a[0][3] = 9.0;
+        Double[][] b = new Double[graph.length][graph.length];
 
-        a[1][0] = 9.0;
-        a[1][1] = 0.0;
-        a[1][2] = 3.0;
-        a[1][3] = 8.0;
+        b[0][0] = 0.0;
+        b[0][1] = 2.0;
+        b[0][2] = 1.0;
+        b[0][3] = 3.0;
+        b[0][4] = 5.0;
 
-        a[2][0] = 6.0;
-        a[2][1] = 8.0;
-        a[2][2] = 0.0;
-        a[2][3] = 5.0;
+        b[1][0] = 4.0;
+        b[1][1] = 0.0;
+        b[1][2] = 1.0;
+        b[1][3] = 2.0;
+        b[1][4] = 3.0;
 
-        a[3][0] = 1.0;
-        a[3][1] = 3.0;
-        a[3][2] = 5.0;
-        a[3][3] = 0.0;
+        b[2][0] = 4.0;
+        b[2][1] = 6.0;
+        b[2][2] = 0.0;
+        b[2][3] = 2.0;
+        b[2][4] = 9.0;
 
-        assertArrayEquals(a, t);
+        b[3][0] = 2.0;
+        b[3][1] = 4.0;
+        b[3][2] = 3.0;
+        b[3][3] = 0.0;
+        b[3][4] = 7.0;
+
+        b[4][0] = 8.0;
+        b[4][1] = 10.0;
+        b[4][2] = 4.0;
+        b[4][3] = 6.0;
+        b[4][4] = 0.0;
+
+        Integer[][] v = new Integer[via.length][via.length];
+
+        v[0][0] = 1;
+        v[0][1] = 1;
+        v[0][2] = 1;
+        v[0][3] = 3;
+        v[0][4] = 2;
+
+        v[1][0] = 4;
+        v[1][1] = 2;
+        v[1][2] = 2;
+        v[1][3] = 2;
+        v[1][4] = 2;
+
+        v[2][0] = 4;
+        v[2][1] = 1;
+        v[2][2] = 3;
+        v[2][3] = 3;
+        v[2][4] = 2;
+
+        v[3][0] = 4;
+        v[3][1] = 1;
+        v[3][2] = 1;
+        v[3][3] = 4;
+        v[3][4] = 2;
+
+        v[4][0] = 4;
+        v[4][1] = 1;
+        v[4][2] = 5;
+        v[4][3] = 3;
+        v[4][4] = 5;
+
+        assertArrayEquals(b, graph);
+        assertArrayEquals(v, via);
     }
 }
