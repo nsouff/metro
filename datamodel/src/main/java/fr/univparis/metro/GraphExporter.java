ba83e46c503115027f3a0150d35e4a622d36b839
Romain Bouarah <Rom.Bou@hotmail.fr>
2020-03-27 13:49:37 +0100

    Fix javadoc in exportToDOT in GraphExporter

--- datamodel/src/main/java/fr/univparis/metro/GraphExporter.java
+++ datamodel/src/main/java/fr/univparis/metro/GraphExporter.java
@@ -10,11 +10,12 @@ public class GraphExporter {
     /**
      * Export a graph to the DOT format.
      * @param graph The graph to export to DOT.
-     * @param path The path where to put the result.
+     * @param output The file in which we write the result.
+     * @param desc The naming function for vertex.
      * @param T The type of vertex.
      */
-    public static <T> void exportToDOT(WGraph<T> graph, File path, Function<T, String> desc) throws IOException {
-	FileWriter fw = new FileWriter(path);
+    public static <T> void exportToDOT(WGraph<T> graph, File output, Function<T, String> desc) throws IOException {
+	FileWriter fw = new FileWriter(output);
 
 	fw.write("digraph");
 	fw.write(" {\n");
