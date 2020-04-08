f2acdbeb29511617cd5e8b0dfb5750bee1024450
Romain Bouarah <Rom.Bou@hotmail.fr>
2020-04-08 19:33:56 +0200

    Add clean data for setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -28,11 +28,39 @@ download_data_zip()
     return $?
 }
 
+extract_data()
+{
+    TMP_WORKING_DIRECTORY="$WORKING_DIRECTORY/TMP"
+    
+    unzip -o "$1" "*METRO_[1-9]*" -d "$TMP_WORKING_DIRECTORY"
+    if [[ $? -ne 0 ]] ; then
+	return 1
+    fi
+
+
+    for LINE_ZIP in `ls $TMP_WORKING_DIRECTORY` ; do
+	LINE=$( echo $LINE_ZIP | cut -d "_" -f 4 | head -c -5)
+	unzip -o "$TMP_WORKING_DIRECTORY/$LINE_ZIP" "stop_times.txt" "stops.txt" -d "$WORKING_DIRECTORY/$LINE"
+    done
+    
+    rm -r "$TMP_WORKING_DIRECTORY"
+}
+
+clean_data()
+{
+    for LINE in `ls -d $1/*/` ; do
+	cut -d , -f 1,3 "$LINE/stops.txt" > "$LINE/stations.txt"
+	cut -d , -f 2,4,5 "$LINE/stop_times.txt" > "$LINE/horaires.txt"
+	rm "$LINE/stops.txt" "$LINE/stop_times.txt"
+    done
+}
+
 echo "Starting setup"
 
+# init working directory variable
+WORKING_DIRECTORY="metro_lines"
 
 #We create working directory
-WORKING_DIRECTORY="metro_lines"
 init_working_directory $WORKING_DIRECTORY
 if [[ $? -ne 0 ]] ; then
     exit 1
@@ -43,21 +71,13 @@ fi
 DATA_ZIP="$WORKING_DIRECTORY/RATP_GTFS_LINES.zip"
 download_data_zip $DATA_ZIP
 if [[ $? -ne 0 ]] ; then
-    exit 2
+    exit 1
 fi
 
 
 # Extract data about metro lines
-TMP_WORKING_DIRECTORY="$WORKING_DIRECTORY/TMP"
-unzip -o $DATA_ZIP "*METRO_[1-9]*" -d $TMP_WORKING_DIRECTORY
-if [[ $? -ne 0 ]] ; then
-    exit 3
-fi
+extract_data $DATA_ZIP
 
-
-for LINE_ZIP in `ls $TMP_WORKING_DIRECTORY` ; do
-    LINE=$( echo $LINE_ZIP | cut -d "_" -f 4 | head -c -5)
-    unzip -o $TMP_WORKING_DIRECTORY/$LINE_ZIP "stop_times.txt" "stops.txt" -d $WORKING_DIRECTORY/$LINE
-done
+clean_data $WORKING_DIRECTORY
 
 echo "Ending setup"
