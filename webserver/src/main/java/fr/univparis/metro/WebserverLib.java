79cb16e2fb5b9d5e4058dc5596247e24c4a01f33
Nathan Souffan <souffan.nathan@gmail.com>
2020-04-10 11:43:54 +0200

    English correction

--- webserver/src/main/java/fr/univparis/metro/WebserverLib.java
+++ webserver/src/main/java/fr/univparis/metro/WebserverLib.java
@@ -66,8 +66,8 @@ public class WebserverLib {
     int minutes5 = minutesTmp5 % 60;
     int hours5 = (minutesTmp5 - minutes5) / 60;
     return "<h3>The longest traject between two stations :</h3>Traject : " + stat1.getObj().getObj() + " to " + stat1.getObj().getValue()
-    + "<br>Time : " + hours + "h "+ minutes + "min " + seconds + "sec<br><h3>Number minimum of correspondence to do all the trajects possible on the network : " + stat2 +"</h3><br><h3> The average number of stations per line : " + stat6 + "</h3><br><h3>The line with the most stations : line " + stat3 + "</h3>"
-    + "<br><h3>The line with the less stations : line " + stat4 + "</h3><br><h3>The average time on the lines : " + hours5 + "h "+ minutes5 + "min " + seconds5 + "sec</h3><br><h3>The longest(duration) line : line " + stat7
+    + "<br>Time : " + hours + "h "+ minutes + "min " + seconds + "sec<br><h3>Number minimum of correspondence to do all the possible trajects on the network : " + stat2 +"</h3><br><h3> The average number of stations per line : " + stat6 + "</h3><br><h3>The line with the most stations : line " + stat3 + "</h3>"
+    + "<br><h3>The line with the least stations : line " + stat4 + "</h3><br><h3>The average time from one terminus of a line to the other : " + hours5 + "h "+ minutes5 + "min " + seconds5 + "sec</h3><br><h3>The longest(duration) line : line " + stat7
     + "</h3><br><h3>The shortest(duration) line : line " + stat8 + "</h3><br>" ;
   }
 
