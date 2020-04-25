97660886108713c1dd69f9311b31dd3d8f629590
Lucas Ketels <lucas.ketels@etu.univ-paris-diderot.fr>
2020-04-25 20:34:35 +0200

    Change the way we have to type the name of the station that are on the begining of fork and cycle for the limitedconnxionsearch algorithm

--- datamodel/src/main/java/fr/univparis/metro/LimitedConnectionSearch.java
+++ datamodel/src/main/java/fr/univparis/metro/LimitedConnectionSearch.java
@@ -64,17 +64,23 @@ public class LimitedConnectionSearch {
     public static ArrayList<Pair<String, String>> getPath(MatriceWGraph g, String start, String end){
         floyd(g.getDirect(), g.getVia(), g.getIntermediate());
         HashMap<String, Integer> h = g.getForkAndCycleStation();
-        //if(h.containsKey(start) && h.containsKey(end)) return getPathForkCycleStartAndEnd(g, start, end);
-        if(h.containsKey(start)) return getPathForkCycleStart(g, start, end);
-        if(h.containsKey(end)) return getPathForkCycleEnd(g, start, end);
-        int numStart = g.getSetOfVertices().get(start);
-        int numEnd = g.getSetOfVertices().get(end);
+        String strStart = start;
+        String strEnd = end;
+        if(h.containsKey(start) && h.containsKey(end)){
+            Pair<String, String> p = startAndEnd(g, start, end);
+            strStart = p.getObj();
+            strEnd = p.getValue();
+        }
+        if(h.containsKey(start) && !h.containsKey(end)) strStart = start(g, start, end);
+        if(h.containsKey(end) && !h.containsKey(start)) strEnd = end(g, start, end);
+        int numStart = g.getSetOfVertices().get(strStart);
+        int numEnd = g.getSetOfVertices().get(strEnd);
         ArrayList<Pair<String, String>> ret = new ArrayList<Pair<String, String>>();
-        String current = end;
+        String current = strEnd;
         String currentLine = "FIN";
         ret.add(new Pair<String, String>(current, currentLine));
         int n = numEnd;
-        while(!start.equals(current)){
+        while(!strStart.equals(current)){
             current = g.getVia()[numStart][n].getName();
             currentLine = g.getVia()[numStart][n].getLine();
             Pair<String, String> p = new Pair<String, String>(current, currentLine);
@@ -85,136 +91,40 @@ public class LimitedConnectionSearch {
     }
 
 
-//The following methods are used to fix the bug of the correspondance times when we go through a fork or a cycle
-
-    private static ArrayList<Pair<String, String>> getPathForkCycleStart(MatriceWGraph g, String start, String end){
+//The following methods are used to avoid typing nameStation + $1-2 when the departure or the arrival is the begining
+//of a fork or a cycle
+    private static String start(MatriceWGraph g, String start, String end){
         String str1 = start + "$1";
         String str2 = start + "$2";
-        ArrayList<Pair<String, String>> a1 = new ArrayList<Pair<String, String>>();
-        int numStart1 = g.getSetOfVertices().get(str1);
-        int numEnd1 = g.getSetOfVertices().get(end);
-        String current1 = end;
-        String currentLine1 = "FIN";
-        a1.add(new Pair<String, String>(current1, currentLine1));
-        int n1 = numEnd1;
-        while(!str1.equals(current1)){
-            current1 = g.getVia()[numStart1][n1].getName();
-            currentLine1 = g.getVia()[numStart1][n1].getLine();
-            Pair<String, String> p = new Pair<String, String>(current1, currentLine1);
-            a1.add(p);
-            n1 = g.getSetOfVertices().get(current1);
-        }
-        ArrayList<Pair<String, String>> a2 = new ArrayList<Pair<String, String>>();
-        int numStart2 = g.getSetOfVertices().get(str2);
-        int numEnd2 = g.getSetOfVertices().get(end);
-        String current2 = end;
-        String currentLine2 = "FIN";
-        a2.add(new Pair<String, String>(current2, currentLine2));
-        int n2 = numEnd2;
-        while(!str2.equals(current2)){
-            current2 = g.getVia()[numStart2][n2].getName();
-            currentLine2 = g.getVia()[numStart2][n2].getLine();
-            Pair<String, String> p = new Pair<String, String>(current2, currentLine2);
-            a2.add(p);
-            n2 = g.getSetOfVertices().get(current2);
-        }
-        return (a1.size() < a2.size()) ? a1 : a2;
+        int nbCorrespondances1 = g.getIntermediate()[g.getSetOfVertices().get(str1)][g.getSetOfVertices().get(end)];
+        Double time1 = g.getDirect()[g.getSetOfVertices().get(str1)][g.getSetOfVertices().get(end)];
+        int nbCorrespondances2 = g.getIntermediate()[g.getSetOfVertices().get(str2)][g.getSetOfVertices().get(end)];
+        Double time2 = g.getDirect()[g.getSetOfVertices().get(str2)][g.getSetOfVertices().get(end)];
+        if(nbCorrespondances1 < nbCorrespondances2 || (nbCorrespondances1 == nbCorrespondances2 && time1 < time2)) return str1;
+        else return str2;
     }
 
-    private static ArrayList<Pair<String, String>> getPathForkCycleEnd(MatriceWGraph g, String start, String end){
+    private static String end(MatriceWGraph g, String start, String end){
         String str1 = end + "$1";
         String str2 = end + "$2";
-        ArrayList<Pair<String, String>> a1 = new ArrayList<Pair<String, String>>();
-        int numStart1 = g.getSetOfVertices().get(start);
-        int numEnd1 = g.getSetOfVertices().get(str1);
-        String current1 = str1;
-        String currentLine1 = "FIN";
-        a1.add(new Pair<String, String>(current1, currentLine1));
-        int n1 = numEnd1;
-        while(!start.equals(current1)){
-            current1 = g.getVia()[numStart1][n1].getName();
-            currentLine1 = g.getVia()[numStart1][n1].getLine();
-            Pair<String, String> p = new Pair<String, String>(current1, currentLine1);
-            a1.add(p);
-            n1 = g.getSetOfVertices().get(current1);
-        }
-        ArrayList<Pair<String, String>> a2 = new ArrayList<Pair<String, String>>();
-        int numStart2 = g.getSetOfVertices().get(start);
-        int numEnd2 = g.getSetOfVertices().get(str2);
-        String current2 = str2;
-        String currentLine2 = "FIN";
-        a2.add(new Pair<String, String>(current2, currentLine2));
-        int n2 = numEnd2;
-        while(!start.equals(current2)){
-            current2 = g.getVia()[numStart2][n2].getName();
-            currentLine2 = g.getVia()[numStart2][n2].getLine();
-            Pair<String, String> p = new Pair<String, String>(current2, currentLine2);
-            a2.add(p);
-            n2 = g.getSetOfVertices().get(current2);
-        }
-        return (a1.size() < a2.size()) ? a1 : a2;
+        int nbCorrespondances1 = g.getIntermediate()[g.getSetOfVertices().get(start)][g.getSetOfVertices().get(str1)];
+        Double time1 = g.getDirect()[g.getSetOfVertices().get(start)][g.getSetOfVertices().get(str1)];
+        int nbCorrespondances2 = g.getIntermediate()[g.getSetOfVertices().get(start)][g.getSetOfVertices().get(str2)];
+        Double time2 = g.getDirect()[g.getSetOfVertices().get(start)][g.getSetOfVertices().get(str2)];
+        if(nbCorrespondances1 < nbCorrespondances2 || (nbCorrespondances1 == nbCorrespondances2 && time1 < time2)) return str1;
+        else return str2;
     }
 
-    private static ArrayList<Pair<String, String>> getPathForkCycleStartAndEnd(MatriceWGraph g, String start, String end){
-        String str1 = start + "$1";
-        String str2 = end + "$1";
-        String str3 = start + "$2";
-        String str4 = start + "$2";
-        ArrayList<Pair<String, String>> a1 = new ArrayList<Pair<String, String>>();
-        int numStart = g.getSetOfVertices().get(str1);
-        int numEnd = g.getSetOfVertices().get(str2);
-        String current = str2;
-        String currentLine = "FIN";
-        a1.add(new Pair<String, String>(current, currentLine));
-        int n = numEnd;
-        while(!str1.equals(current)){
-            current = g.getVia()[numStart][n].getName();
-            currentLine = g.getVia()[numStart][n].getLine();
-            a1.add(new Pair<String, String>(current, currentLine));
-            n = g.getSetOfVertices().get(current);
+    private static Pair<String, String> startAndEnd(MatriceWGraph g, String start, String end){
+        String start1 = start(g, start, end + "$1");
+        String start2 = start(g, start, end + "$2");
+        int nbCorrespondances1 = g.getIntermediate()[g.getSetOfVertices().get(start1)][g.getSetOfVertices().get(end + "$1")];
+        Double time1 = g.getDirect()[g.getSetOfVertices().get(start1)][g.getSetOfVertices().get(end + "$1")];
+        int nbCorrespondances2 = g.getIntermediate()[g.getSetOfVertices().get(start2)][g.getSetOfVertices().get(end + "$2")];
+        Double time2 = g.getDirect()[g.getSetOfVertices().get(start2)][g.getSetOfVertices().get(end + "$2")];
+        if(nbCorrespondances1 < nbCorrespondances2 || (nbCorrespondances1 == nbCorrespondances2 && time1 < time2)){
+            return new Pair<String, String>(start1, end + "$1");
         }
-        ArrayList<Pair<String, String>> a2 = new ArrayList<Pair<String, String>>();
-        numStart = g.getSetOfVertices().get(str3);
-        numEnd = g.getSetOfVertices().get(str2);
-        current = str2;
-        currentLine = "FIN";
-        a1.add(new Pair<String, String>(current, currentLine));
-        n = numEnd;
-        while(!str1.equals(current)){
-            current = g.getVia()[numStart][n].getName();
-            currentLine = g.getVia()[numStart][n].getLine();
-            a1.add(new Pair<String, String>(current, currentLine));
-            n = g.getSetOfVertices().get(current);
-        }
-        ArrayList<Pair<String, String>> a3 = new ArrayList<Pair<String, String>>();
-        numStart = g.getSetOfVertices().get(str1);
-        numEnd = g.getSetOfVertices().get(str4);
-        current = str4;
-        currentLine = "FIN";
-        a1.add(new Pair<String, String>(current, currentLine));
-        n = numEnd;
-        while(!str1.equals(current)){
-            current = g.getVia()[numStart][n].getName();
-            currentLine = g.getVia()[numStart][n].getLine();
-            a1.add(new Pair<String, String>(current, currentLine));
-            n = g.getSetOfVertices().get(current);
-        }
-        ArrayList<Pair<String, String>> a4 = new ArrayList<Pair<String, String>>();
-        numStart = g.getSetOfVertices().get(str3);
-        numEnd = g.getSetOfVertices().get(str4);
-        current = str4;
-        currentLine = "FIN";
-        a1.add(new Pair<String, String>(current, currentLine));
-        n = numEnd;
-        while(!str1.equals(current)){
-            current = g.getVia()[numStart][n].getName();
-            currentLine = g.getVia()[numStart][n].getLine();
-            a1.add(new Pair<String, String>(current, currentLine));
-            n = g.getSetOfVertices().get(current);
-        }
-        if(a1.size() < a2.size() && a1.size() < a3.size() && a1.size() < a4.size()) return a1;
-        if(a2.size() < a1.size() && a2.size() < a3.size() && a2.size() < a4.size()) return a2;
-        if(a3.size() < a1.size() && a3.size() < a2.size() && a3.size() < a4.size()) return a3;
-        else return a4;
+        else return new Pair<String, String>(start2, end + "$2");
     }
 }
