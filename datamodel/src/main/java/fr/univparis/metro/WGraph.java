3c26d777e8e5363cc416b664b93d88285ddb3b44
Nathan Souffan <souffan.nathan@gmail.com>
2020-05-07 23:56:55 +0200

    Fix javadoc in WGraph

--- datamodel/src/main/java/fr/univparis/metro/WGraph.java
+++ datamodel/src/main/java/fr/univparis/metro/WGraph.java
@@ -3,19 +3,23 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.function.Predicate;
 
+/**
+ * A weighted graph class
+ */
 public class WGraph<T> implements Cloneable{
 
   private HashMap<T, HashMap<T, Double>> wGraph;
 
   /**
-  * Construct a WGraph with a corresponding hashmap for wGraph
+  * Creates an empty Weighted Graph
   */
   public WGraph(){
     this.wGraph = new HashMap<T, HashMap<T, Double>>();
   }
 
   /**
-  * Create a cloning WGraph by the WGraph
+  * Creates a clone of the graph.
+  * Vertices will not be clone
   * @return a clone of the WGraph
   */
   public WGraph<T> clone(){
@@ -32,31 +36,31 @@ public class WGraph<T> implements Cloneable{
   }
 
   /**
-  * Compare the WGraph g with the WGraph
+  * Returns true if and only if the instance graph and the parameter graph represent the same network
   * @param g the WGraph to compare with the WGraph
-  * @return true if there are equals else false
+  * @return True if and only if the instance graph and the parameter graph represent the same network
   */
   public boolean equals(WGraph<T> g){
     return(g.wGraph.equals(this.wGraph));
   }
 
   /**
-  * Return the set of all the keys contained in the wGraph of WGraph
-  * @return the set of all the keys contained in the HashMap wGraph.
+  * Returns a set of all the vertices in the graph.
+  * @return a set of all the vertices in the graph.
   */
   public Set<T> getVertices(){
     return this.wGraph.keySet();
   }
   /**
-  * Return the number of vertices of the WGraph
+  * Returns the number of vertices of the WGraph
   * @return the number of vertices of the WGraph
   */
   public int nbVertex() {return wGraph.size();}
 
   /**
-  * Give the value of a key T vertex in wGraph of the WGraph
-  * @param vertex the vertex of which we want the neighbors
-  * @return a list that contains all the vertices we can reach from the vertex "vertex".
+  * Returns the neighbors of a vertex contained in the graph.
+  * @param vertex the vertex which we want the neighbors.
+  * @return the set of all the vertices that can be reach by the parameter vertex
   */
   public Set<T> neighbors(T vertex){
     return wGraph.get(vertex).keySet();
@@ -64,9 +68,9 @@ public class WGraph<T> implements Cloneable{
 
 
   /**
-  * Return the weight between two vertices
-  * @param s a vertex we want find the weight with the vertex p
-  * @param p the other vertex
+  * Returns the weight between two vertices
+  * @param s the start of the edge
+  * @param p the end of the edge
   * @return the weight between the vertex s and the vertex p.
   */
   public Double weight(T s, T p){
@@ -77,9 +81,9 @@ public class WGraph<T> implements Cloneable{
 
 
   /**
-  * Add a vertex to the WGraph and return if it is good after that
-  * @param v a vertex we add to the HashMap wGraph.
-  * @return true if v has correctly been added, false otherwise.
+  * Adds a vertex to the graph.
+  * @param v the vertex we want to add.
+  * @return true if v has been added.
   */
   public boolean addVertex(T v){
     if(this.wGraph.get(v) == null){
@@ -91,9 +95,9 @@ public class WGraph<T> implements Cloneable{
 
 
   /**
-  * Delete a vertex v in the wGraph of the WGraph
-  * @param v a vertex we delete from the HashMap wGraph.
-  * @return true if v has been correctly deleted, false otherwise.
+  * Deletes a vertex of the graph and all the edges that are link to it
+  * @param v the vertex we want to delete
+  * @return true if v has been deleted, false if the vertex hasn't be found.
   */
   public boolean deleteVertex(T v){
     if(this.wGraph.containsKey(v)){
@@ -108,11 +112,11 @@ public class WGraph<T> implements Cloneable{
 
 
   /**
-  * Create and add an edge between two vertices with a weight
+  * Adds an edge between two vertices
   * @param s the start of the edge.
   * @param p the end of the edge.
   * @param weight the weight of the edge.
-  * @return true if the edge has correctly been created and added to the HashMap wGraph.
+  * @return true if the edge has correctly been added, else false (occurs when one of the vertex is vertex is not in this graph or that there is already an edge).
   */
   public boolean addEdge(T s, T p, Double weight){
     if (s.equals(p)) return false;
@@ -125,21 +129,21 @@ public class WGraph<T> implements Cloneable{
 
 
   /**
-  * Remove the vertex that has the vertices s and p.
-  * @param s one of the vertex of the edge we want to remove.
-  * @param p one of the vertex of the edge we want to remove.
-  * @return true if the edge has correctly been removed.
+  * Removes an edge
+  * @param s the start of the edge.
+  * @param p the end of the edge.
+  * @return true if the edge has correctly been removed (false occurs when the specified edge don't exist).
   */
   public boolean removeEdge(T s, T p){
     return wGraph.get(s).remove(p) != null;
   }
 
   /**
-  * Add double edge between e and every element of the graph that evaluate true with p
-  * @param e the vertex wich we want to add Edge
-  * @param weight the weight for the added Edge
-  * @param p the Predicate we want that evaluate if we want to add an edge or not for every element of the graph
-  * @return true if we add an edge, false otherwise
+  * Adds double edge between the specified vertex and all the other vertices that satifies a condition
+  * @param e one end of every added edge
+  * @param weight the weight for the added edges
+  * @param p an edge between the specified vertex and any other edges will be added if the other vertex satifiy this predicate
+  * @return true if and only if the specified vertex is in the graph, else no edge will be add.
   */
   public boolean addDoubleEdge(T e, Double weight, Predicate<T> p) {
     if (!wGraph.containsKey(e)) return false;
@@ -154,8 +158,8 @@ public class WGraph<T> implements Cloneable{
   }
 
   /**
-  * Return the number of Vertex that evaluate true with predicate p
-  * @param p The predicate to test for every vertex
+  * Return the number of Vertices that satifies a condition
+  * @param p The condition for every vertex
   * @return The number of vertex that evaluate true with predicate p
   *
   */
@@ -169,7 +173,7 @@ public class WGraph<T> implements Cloneable{
   }
 
   /**
-  * Return a String containing the toString funtion of every vertex that evualuate true with predicate p
+  * Return a String containing the {@link java.lang.Object#toString} funtion of every vertex that evualuate true with predicate p
   * @param p The predicate to test with every vertex
   * @return a String containing the toString funtion of every vertex that evualuate true with predicate p
   *
@@ -183,19 +187,19 @@ public class WGraph<T> implements Cloneable{
   }
 
   /**
-  * Verify if the WGraph contains a vertex s (in his keys)
-  * @param s the vertex we want know the presence in the WGraph
-  * @return true if it contains s else false
+  * Inidicates the presence of the specified vertex in the graph
+  * @param s the vertex we want know the presence in the graph
+  * @return true if and only if the specified vertex is in the graph
   */
   public boolean containsVertex(T s){
     return wGraph.containsKey(s);
   }
 
   /**
-  * Modify the weight between two vertices
-  * @param s      the first vertex
-  * @param p      the second vertex
-  * @param weight the weight that we want to put between these two vertices
+  * Modifies the weight between two vertices
+  * @param s the start of the edge
+  * @param p the end of the edge
+  * @param weight the new weight of the edge
   */
   public void setWeight(T s, T p, Double weight) {
     if (! wGraph.get(s).containsKey(p)) return;
@@ -203,11 +207,13 @@ public class WGraph<T> implements Cloneable{
   }
 
  /**
-  * Add or modify the vertices that are in g into the instance object
-  * Previous weight will be overwritten
+  * Adds and modifies the vertices and eges that are in the specified graph into the instance graph.
+  * If a vertex of the specified graph is not in the instance graph it will be added.
+  * Same for the edges.
+  * If an edges between is in both graph the weight in the instance graph will be modify with the weight in the specified graph.
   * @param g the graph containing all the modification that we want to apply to the isntance graph
   */
-  public void apply( WGraph<T> g) {
+  public void apply(WGraph<T> g) {
     for (T t : g.getVertices()) {
       if (! this.containsVertex(t)) this.addVertex(t);
       for (T n : g.neighbors(t)) {
@@ -216,9 +222,10 @@ public class WGraph<T> implements Cloneable{
       }
     }
   }
+
  /**
-  * Remove a vertex and replace it by two vertex wich will have the same edges
-  * @param old the vertex we want to split in two vertices
+  * Removes a vertex and replace it by two vertex wich will have the same edges.
+  * @param old the vertex we want to split into two vertices
   * @param newVert1 one of the new vertex that replace old
   * @param newVert2 the other new vertex that replace old
   */
